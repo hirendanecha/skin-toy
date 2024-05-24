4b02f1ba31c1afa71ce79446eaff416a4e8cbaa6
prashant <ppo.opash@gmail.com>
2024-05-24 15:47:39 +0530

    allow to show email on registration process

--- src/app/layouts/auth-layout/pages/sign-up/sign-up.component.ts
+++ src/app/layouts/auth-layout/pages/sign-up/sign-up.component.ts
@@ -172,7 +172,7 @@ export class SignUpComponent implements OnInit, AfterViewInit {
             this.submitted = true;
             this.type = 'success';
             this.registrationMessage =
-              'Please check your email and click the link to activate your account.';
+            `Email sent to ${this.registerForm.value.Email}, Please check your email and click the link to activate your account.`;
             this.scrollTop();
             this.isragister = true;
             const userData = data.data;
